ed29a18feb06530ee22d87d38e98cbf93fae5bdc
amandats4i <amandatsai16@gmail.com>
2023-12-11 13:00:30 -0300

    Inserção dos métodos GET, POST, PUT e DELETE

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,16 +1,27 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping ("/postagens")
@@ -27,4 +38,53 @@ public class PostagemController {
 		
 	}
 
+	//O que está dentro da chave é uma variável que receberá um valor e será usada pelo metodo
+	@GetMapping ("/{id}")
+	public ResponseEntity<Postagem> getById (@PathVariable Long id){
+		return postagemRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta)) // O Resultado do método fica salvo na variavel "resposta"
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		
+		// A função acima é equivalente a linha do MySQL: SELECT * FROM tb_postagens WHERE id = ?;
+		
+	}
+	
+	//MÉTODO PARA BUSCA PERSONALIZADA DO TITULO
+	@GetMapping ("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo (@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> post (@Valid @RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+		
+		// A linha acima é equivalente a INSERT INTO tb_postagens (titulo, texto) VALUES (?,?) do SQL;
+	}
+	
+	@PutMapping 
+	public ResponseEntity<Postagem> put (@Valid @RequestBody Postagem postagem){
+		return postagemRepository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+						.body(postagemRepository.save(postagem)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+				
+		
+		// A linha acima é equivalente a UPDATE tb_postagens SET titulo = ? texto = ? WHERE id = ? do SQL;
+	}
+	
+	@DeleteMapping ("/{id}") 
+	@ResponseStatus(HttpStatus.NO_CONTENT) //Aqui a gente força o retorno do status "204 - No Content"
+	public void delete (@PathVariable Long id) {
+		
+		Optional <Postagem> postagem = postagemRepository.findById(id);
+		
+		if (postagem.isEmpty()) 
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		postagemRepository.deleteById(id);
+		
+		//É equivalente ao SQL DELETE FROM tb_postagens WHERE id = ?;
+	}
+	
 }
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,10 +1,22 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Postagem;
 
 //JpaRepository é uma interface já existente no Spring que importamos e setamos dois parametros: a Classe com a qual ela vai interagir (Postagem) e a chave primaria dessa classe (do tipo Long).
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
+	
+	// BUSCA PERSONALIZADA
+	List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+
+	// A linha acima é equivalente em SQL a SELECT * FROM tb_postagens WHERE titulo
+	// LIKE "%?%"
+	
+	// Utilizamos o @Param sempre quando estamos buscando por uma String. Ele torna
+	// a String titulo um parâmetro que vai receber o que estiver dentro das aspas.
 }
