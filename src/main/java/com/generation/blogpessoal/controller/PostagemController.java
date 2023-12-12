866639809f6218d5bfff6c78503d69f934a1c019
amandats4i <amandatsai16@gmail.com>
2023-12-12 17:04:29 -0300

    Relacionamento entre Postagem e Tema

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -20,71 +20,89 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping ("/postagens")
-@CrossOrigin (origins = "*", allowedHeaders = "*") /* Temos que definir o CrossOrigin para liberar requisições de outros servidores quqe vão acessar o front-end*/
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*") /*
+													 * Temos que definir o CrossOrigin para liberar requisições de
+													 * outros servidores quqe vão acessar o front-end
+													 */
 
 public class PostagemController {
-	
+
 	@Autowired
 	private PostagemRepository postagemRepository;
-	
+
+	@Autowired
+	private TemaRepository temaRepository;
+
 	@GetMapping
-	public ResponseEntity <List <Postagem>> getAll () {
-		return ResponseEntity.ok(postagemRepository.findAll()); // o findAll é equivalente ao: SELECT * FROM tb_postagens;
-		
+	public ResponseEntity<List<Postagem>> getAll() {
+		return ResponseEntity.ok(postagemRepository.findAll()); // o findAll é equivalente ao: SELECT * FROM
+																// tb_postagens;
+
 	}
 
-	//O que está dentro da chave é uma variável que receberá um valor e será usada pelo metodo
-	@GetMapping ("/{id}")
-	public ResponseEntity<Postagem> getById (@PathVariable Long id){
+	// O que está dentro da chave é uma variável que receberá um valor e será usada
+	// pelo metodo
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id) {
 		return postagemRepository.findById(id)
 				.map(resposta -> ResponseEntity.ok(resposta)) // O Resultado do método fica salvo na variavel "resposta"
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-		
-		// A função acima é equivalente a linha do MySQL: SELECT * FROM tb_postagens WHERE id = ?;
-		
+
+		// A função acima é equivalente a linha do MySQL: SELECT * FROM tb_postagens
+		// WHERE id = ?;
+
 	}
-	
-	//MÉTODO PARA BUSCA PERSONALIZADA DO TITULO
-	@GetMapping ("/titulo/{titulo}")
-	public ResponseEntity<List<Postagem>> getByTitulo (@PathVariable String titulo){
+
+	// MÉTODO PARA BUSCA PERSONALIZADA DO TITULO
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
 		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Postagem> post (@Valid @RequestBody Postagem postagem){
-		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
-		
-		// A linha acima é equivalente a INSERT INTO tb_postagens (titulo, texto) VALUES (?,?) do SQL;
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
+		if (temaRepository.existsById(postagem.getTema().getId()))
+			return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe.", null);
+
+		// A linha acima é equivalente a INSERT INTO tb_postagens (titulo, texto) VALUES
+		// (?,?) do SQL;
 	}
-	
+
 	@PutMapping 
 	public ResponseEntity<Postagem> put (@Valid @RequestBody Postagem postagem){
-		return postagemRepository.findById(postagem.getId())
-				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
-						.body(postagemRepository.save(postagem)))
-				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		
+		if (postagemRepository.existsById(postagem.getId())) {
 				
+			if (temaRepository.existsById(postagem.getTema().getId())) 
+				return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save((postagem)));
+				
+			 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe.", null);
+			
+		}
 		
-		// A linha acima é equivalente a UPDATE tb_postagens SET titulo = ? texto = ? WHERE id = ? do SQL;
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();	
+		
 	}
-	
-	@DeleteMapping ("/{id}") 
-	@ResponseStatus(HttpStatus.NO_CONTENT) //Aqui a gente força o retorno do status "204 - No Content"
-	public void delete (@PathVariable Long id) {
-		
-		Optional <Postagem> postagem = postagemRepository.findById(id);
-		
-		if (postagem.isEmpty()) 
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT) // Aqui a gente força o retorno do status "204 - No Content"
+	public void delete(@PathVariable Long id) {
+
+		Optional<Postagem> postagem = postagemRepository.findById(id);
+
+		if (postagem.isEmpty())
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-		
+
 		postagemRepository.deleteById(id);
-		
-		//É equivalente ao SQL DELETE FROM tb_postagens WHERE id = ?;
+
+		// É equivalente ao SQL DELETE FROM tb_postagens WHERE id = ?;
 	}
-	
+
 }
--- src/main/java/com/generation/blogpessoal/model/Tema.java
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -1,26 +1,43 @@
 package com.generation.blogpessoal.model;
 
-import jakarta.persistence.Entity;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "tb_tema")
 
 public class Tema {
-	
-	@Id // 
+
+	@Id //
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
 
-	@NotBlank(message = "O Atributo descrição é obrigatório!")  
+	@NotNull(message = "O Atributo descrição é obrigatório!")
 	private String descricao;
+
+	// Aqui, estamos estabelecendo a relação entre as tabelas.
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "tema", cascade = CascadeType.REMOVE) 
+	@JsonIgnoreProperties("tema") // Ele salva uma vez e ignora o tema.
+	
+	// O fetch é uma forma de carregar os dados de um bd durante uma busca. 
+	// Usamos o modo Lazy pois ele carrega os dados mas nao carrega o tema até seja estritamente
+	// solicitado. O cascade faz cascateamento dos dados. Todas as vezes que eu
+	// apagar um tema, ele apaga tudo o que está dentro dele.
+	// Mapped By serve para indicar qual objeto irá identificar sua chave estrangeira.
+	
+	
+	private List<Postagem> postagem;
 
 	public Long getId() {
 		return id;
@@ -37,7 +54,13 @@ public class Tema {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	
-	
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
 
 }
